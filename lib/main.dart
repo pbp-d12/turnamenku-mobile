a7a1dc395d6900f02328e7e75099a0338af33e9a
mugmold <mugmold@gmail.com>
2025-11-24 14:43:29 +0700

    update?

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pbp_django_auth/pbp_django_auth.dart'; 
-import 'package:provider/provider.dart'; 
 
 void main() {
   runApp(const MyApp());
