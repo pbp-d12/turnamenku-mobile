e4999b37eaa106b1e8688a72e1ac7571ce75cb36
unknown <gibran.021105@gmail.com>
2025-11-24 14:16:03 +0700

    adding dependencies for django auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart'; 
+import 'package:provider/provider.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Turnamenku',
       theme: ThemeData(
         // This is the theme of your application.
         //
